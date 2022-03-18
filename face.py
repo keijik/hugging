e47a1eae340498fd3a40ecb6844b44b14c786e43
keijik <keijik>
2022-03-18 19:48:51 +0000

    add face noetbook

---
+++ face.py
@@ -0,0 +1,36 @@
+# Databricks notebook source
+import transformers
+
+# COMMAND ----------
+
+# MAGIC %pip install transformers
+
+# COMMAND ----------
+
+import transformers
+
+# COMMAND ----------
+
+# MAGIC %pip install torch
+
+# COMMAND ----------
+
+import datasets
+
+# COMMAND ----------
+
+# MAGIC %pip install datasets
+
+# COMMAND ----------
+
+# MAGIC %pip install pyarrow
+
+# COMMAND ----------
+
+from datasets import list_datasets, list_metrics, load_dataset, load_metric
+
+from pprint import pprint
+
+# COMMAND ----------
+
+
